2bc03f619804ff1f5500118bf741a288197d1cda
Javier Molina <j.sheerp@gmail.com>
2021-09-13 19:57:53 +0200

    initial

---
+++ src/test/java/estest/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package estest.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
